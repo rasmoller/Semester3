a70a8ebdd5c8dd09ce4762cc995d9c2bb77fd704
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2020-09-28 14:45:58 +0200

    LEK5

--- DOA/LEK5/i3doa-sorting/i3doa-sorting/Quicksort.h
+++ DOA/LEK5/i3doa-sorting/i3doa-sorting/Quicksort.h
@@ -1,18 +1,66 @@
 #pragma once
+#include <time.h>
 
 // This the version of quicksort that actually performs partitioning, recursive calls, etc. 
 template<typename T>
 void quicksort(T* ar, int start, int end)
 {
-  
-    // if (base case reached) return;  // Base case: No sorting necessary 
 
-    // select pivot index and position pivot;
-    // partition ar[start; end];
+	if (start == end) return;  // Base case: No sorting necessary 
 
-    // Assume: Pivot is at index k after partitioning 
-    //quicksort(ar, start, k - 1);
-    //quicksort(ar, k + 1, end);
+	// select pivot index and position pivot;
+	//partition ar[start; end];
+	int three[3];
+	for (int i = 0; i < 3; i++)
+	{
+		three[i] = rand() % (end - start);
+		three[i] += start;
+	}
+	// Check for median of three
+	int pivot = three[0] > three[1] ? three[0] : three[1];
+	pivot = pivot < three[2] ? pivot : three[2];
+	//Swap Pivot to end index
+	T temp = ar[end];
+	ar[end] = ar[pivot];
+	ar[pivot] = temp;
+	//pivot = end;
+	//Setting wall and i
+	int wall = start;
+	int i = start + 1;
+
+	while (i != pivot)
+	{
+		if (ar[pivot] > ar[wall])
+		{
+			if (ar[pivot] > ar[i])
+			{
+				wall++;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		else
+		{
+			if (ar[pivot] > ar[i])
+			{
+				T temp = ar[i];
+				ar[i] = ar[wall];
+				ar[wall] = temp;
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+	temp = ar[wall + 1];
+	ar[wall + 1] = ar[pivot];
+	ar[pivot] = temp;
+	// Assume: Pivot is at index k after partitioning 
+	quicksort(ar, start, pivot - 1);
+	quicksort(ar, pivot + 1, end);
 }
 
 
@@ -21,8 +69,21 @@ void quicksort(T* ar, int start, int end)
 template<typename T>
 void quicksort(T* ar, int n)
 {
-    // Preprocess ar (if required)
+	// Preprocess ar (if required)
+	if (n < 2) {
+		return;
+	}
+	if (n == 2)
+	{
+		if (ar[0] > ar[1])
+		{
+			T temp = ar[1];
+			ar[1] = ar[0];
+			ar[0] = temp;
+		}
+		return;
+	}
 
-    // Call Quicksort
-    quicksort(ar, 0, n - 1);
+	// Call Quicksort
+	quicksort(ar, 0, n - 1);
 }
