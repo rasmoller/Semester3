6ea907167c0da3fa3b4934e264e6874f4492e50f
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2020-10-20 09:56:33 +0200

    f

--- DOA/LEK7/i3doa-bst/BST/BST.h
+++ DOA/LEK7/i3doa-bst/BST/BST.h
@@ -23,23 +23,85 @@ private:
 	// Recursive function to find Node with data = data
 	Node<T>* findNode(Node<T>* itsRoot, const T& data)
 	{
-		/* Function to be implemented */ return nullptr;
+		if (itsRoot == nullptr)
+		{
+			return nullptr;
+		}
+		if (itsRoot->data == data)
+		{
+			return itsRoot;
+		}
+		else if (itsRoot->data > data)
+		{
+			return findNode(itsRoot->left, data);
+		}
+		else if (itsRoot->data < data)
+		{
+			return findNode(itsRoot->right, data);
+		}
 	}
 
 	// Recursive function to insert node with data
 	Node<T>* insertNode(Node<T>* itsRoot, const T& data)
 	{
-		/* Function to be implemented */ return nullptr;
+		// Hvad hvis root er nullptr ???
+		if (itsRoot == nullptr)
+		{
+			root = new Node<T>(data, nullptr, nullptr, nullptr);
+			return itsRoot;
+		}
+		if(itsRoot->data >= data)
+		{
+			if (itsRoot->left == nullptr)
+			{
+				itsRoot->left = new Node<T>(data, nullptr, nullptr, itsRoot);
+				return itsRoot->left;
+			} 
+			else
+			{
+				return insertNode(itsRoot->left,data);
+			}
+		}
+		else if (itsRoot->data < data)
+		{
+			if (itsRoot->right == nullptr)
+			{
+				itsRoot->right = new Node<T>(data, nullptr, nullptr, itsRoot);
+				return itsRoot->right;
+			} 
+			else
+			{
+				return insertNode(itsRoot->right, data);
+			}
+		}
+		
+		return nullptr;
 	}
 
 	// Recursive function to find right-most node
 	Node<T>* findRightmostChild(Node<T>* itsRoot) {
-		/* Helper function can be implemented */ return nullptr;
+		if(itsRoot == nullptr)
+		{
+			return nullptr;
+		}
+		if(itsRoot->right != nullptr)
+		{
+			findRightmostChild(itsRoot->right);
+		}
+		return itsRoot;
 	}
 
 	// Recursive function to find left-most node
 	Node<T>* findLeftmostChild(Node<T>* itsRoot) {
-		/* Helper function can be implemented */ return nullptr;
+		if(itsRoot == nullptr)
+		{
+			return nullptr;
+		}
+		if(itsRoot->left != nullptr)
+		{
+			findRightmostChild(itsRoot->left);
+		}
+		return itsRoot;
 	}
 
 
@@ -63,7 +125,45 @@ public:
 
 	void remove(const T& data)
 	{
-		/* Function to be implemented */
+		Node<T> * searchNode = findNode(root, data);
+		if( searchNode != nullptr)
+		{
+			Node<T> * nearestChild = findRightmostChild(searchNode->left);
+			if(nearestChild == nullptr)
+			{
+				nearestChild = findLeftmostChild(searchNode->right);
+				if(nearestChild == nullptr)
+				{
+					if(searchNode->parent == nullptr)
+					{
+						root = nullptr;
+					}
+					else
+					{
+						if(searchNode->parent->left->data == searchNode->data)
+						{
+							searchNode->parent->left = nullptr;
+						}
+						else
+						{
+							searchNode->parent->right = nullptr;
+						}
+					}
+				}
+			}
+			if(nearestChild != nullptr)
+			{
+				searchNode->data = nearestChild->data;
+				if (nearestChild->parent == searchNode)
+				{
+					searchNode->left = nearestChild->left;
+				}
+				else
+				{
+					nearestChild->parent->right = nearestChild->left;
+				}
+			}			
+		}
 	}
 
 };
